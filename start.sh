8ac287746a1a92ba805fd29f0587c4280190d099
Su6666666 <86417238+Su6666666@users.noreply.github.com>
2024-04-16 16:53:49 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Joelkb/DQ-the-file-donor.git /DQTheFileDonorBot
+  git clone https://github.com/Joelkb/Auto-filter-bot-heroku.git /Auto-filter-bot-heroku
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQTheFileDonorBot
+  git clone $UPSTREAM_REPO /Auto-filter-bot-heroku
 fi
 cd /DQTheFileDonorBot
 pip3 install -U -r requirements.txt
-echo "Starting DQ-The-File-Donor...."
+echo "Starting Auto-filter-bot-heroku...."
 python3 bot.py
