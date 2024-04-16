9c22f5e27580db797537d03cce472d3b6f79532d
Su6666666 <86417238+Su6666666@users.noreply.github.com>
2024-04-16 17:06:49 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Joelkb/Auto-filter-bot-heroku.git /Auto-filter-bot-heroku
+  git clone https://github.com/Su6666666/Auto-filter-bot-heroku.git /Auto-filter-bot-heroku
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Auto-filter-bot-heroku
